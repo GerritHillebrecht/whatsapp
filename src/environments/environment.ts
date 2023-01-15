1fc639f898de2677ccd8602510556559f6e8091c
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-01-16 00:40:54 +0100

    Firebase action deploys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'soundnite',
-    appId: '1:844512445986:web:ff3a25bceb4288f871ec7b',
-    storageBucket: 'soundnite.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyDCWSufUcbTC0zRt1oob-7jgeX6OQ_1yHE',
-    authDomain: 'soundnite.firebaseapp.com',
-    messagingSenderId: '844512445986',
-    measurementId: 'G-W50SH2DZF2',
-  },
   production: false,
   graphql: {
     protocol: {

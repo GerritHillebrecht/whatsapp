97cb2f6b366aad980ab415fd52376244a0ea6032
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-03 18:58:36 +0100

    Added the SnackbarModule to the core imports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,11 @@ import { FirebaseModule } from './config/firebase';
 import { NgxsConfigModule } from './config/ngxs';
 import { GraphQLModule } from './config/graphql';
 
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [FirebaseModule, NgxsConfigModule, GraphQLModule],
+  exports: [FirebaseModule, NgxsConfigModule, GraphQLModule, MatSnackBarModule],
 })
 export class CoreModule {}

8d950ea50f1c5cad17a1d25f5758d7bab636e2b1
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-03 19:02:29 +0100

    started the subscription to read updates, still wip

--- src/app/pages/whatsapp/service/subscription/read-status/read-update-subscription.service.ts
+++ src/app/pages/whatsapp/service/subscription/read-status/read-update-subscription.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { WhatsappMessageQueryDto } from '@whatsapp/interface';
+import { Apollo } from 'apollo-angular';
+import { filter, map, Observable, skip, takeUntil } from 'rxjs';
+import {
+  StatusUpdateSubResult,
+  StatusUpdateSubVariables,
+  READ_UPDATE_SUBSCRIPTION,
+} from './read-update.graphql';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReadUpdateSubscriptionService {
+  private takeUntilCondition = this.store
+    .select(({ authentication }) => authentication.whatsappUser)
+    .pipe(skip(1));
 
-  constructor() { }
+  constructor(private apollo: Apollo, private store: Store) {}
+
+  readStatusUpdateSubscription(
+    id: number
+  ): Observable<WhatsappMessageQueryDto> {
+    const subQueryOptions = {
+      query: READ_UPDATE_SUBSCRIPTION,
+      variables: { id },
+    };
+
+    return this.apollo
+      .subscribe<StatusUpdateSubResult, StatusUpdateSubVariables>(
+        subQueryOptions
+      )
+      .pipe(
+        takeUntil(this.takeUntilCondition),
+        filter(({ data }) => Boolean(data)),
+        map(({ data }) => data!.messageStatusSubscription)
+      );
+  }
 }

105ce0a05c3021327bf9cda9c3b1ba42ec64db92
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-03 18:59:10 +0100

    imported the necessary components for the restructuring of the landing page

--- src/app/pages/landing/layout/landing-layout.component.ts
+++ src/app/pages/landing/layout/landing-layout.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainToolbarComponent } from '@shared/ui/navigation/main-toolbar';
+import { LibrariesSectionComponent } from '../sections/libraries-section/libraries-section.component';
+import { PhoneMaskSectionComponent } from '../sections/phone-mask-section/phone-mask-section.component';
 
 @Component({
   selector: 'app-landing-layout',
   standalone: true,
-  imports: [CommonModule, MainToolbarComponent],
+  imports: [
+    CommonModule,
+    MainToolbarComponent,
+    PhoneMaskSectionComponent,
+    LibrariesSectionComponent,
+  ],
   templateUrl: './landing-layout.component.html',
   styleUrls: ['./landing-layout.component.scss'],
 })

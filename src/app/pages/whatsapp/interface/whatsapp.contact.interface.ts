85a4e1a90d4231c9a90d438e536afa48ad65e2f1
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-09 12:45:53 +0100

    Contacts no longer have messages property

--- src/app/pages/whatsapp/interface/whatsapp.contact.interface.ts
+++ src/app/pages/whatsapp/interface/whatsapp.contact.interface.ts
@@ -2,7 +2,7 @@ import { WhatsappMessage } from './whatsapp.message.interface';
 import { WhatsappUser } from './whatsapp.user.interface';
 
 export interface WhatsappContact extends WhatsappUser {
-  messages: WhatsappMessage[];
+  // messages: WhatsappMessage[];
   lastMessage: WhatsappMessage | null;
   unreadMessages: number;
 }

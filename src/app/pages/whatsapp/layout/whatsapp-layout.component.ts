d375b0007a4889ce10ecc4756da1209e31852bdd
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-09 12:47:49 +0100

    removed unused imports and variables

--- src/app/pages/whatsapp/layout/whatsapp-layout.component.ts
+++ src/app/pages/whatsapp/layout/whatsapp-layout.component.ts
@@ -1,22 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ScreenSizeService } from '@core/services/screen-size';
-import { Select } from '@ngxs/store';
-import { WhatsappMessage } from '@whatsapp/interface';
-import { WhatsappState } from '@whatsapp/store';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-whatsapp-layout',
   templateUrl: './whatsapp-layout.component.html',
   styleUrls: ['./whatsapp-layout.component.scss'],
 })
-export class WhatsappLayoutComponent implements OnInit {
-  @Select(WhatsappState.messages)
-  messages$: Observable<WhatsappMessage[]> | undefined;
-
+export class WhatsappLayoutComponent {
   constructor(protected screenSizeService: ScreenSizeService) {}
-
-  ngOnInit(): void {
-    this.messages$?.subscribe((messages) => console.log('%cmessages', 'color: green; font-size: 30px;', messages));
-  }
 }

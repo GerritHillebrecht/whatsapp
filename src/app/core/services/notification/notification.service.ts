f832b932b7445704335dbdfc538597786588f18a
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-09 12:40:47 +0100

    Added general Serverconnection Notification Method

--- src/app/core/services/notification/notification.service.ts
+++ src/app/core/services/notification/notification.service.ts
@@ -8,4 +8,13 @@ export class NotificationService {
   public notification$: Subject<string> = new Subject();
 
   constructor() {}
+
+  serverConnectionError(attempt: number = 0, max: number = 5): void {
+    console.log('Server connection error', attempt, max);
+    const message =
+      attempt < max
+        ? `Es konnte keine Verbindung zum Server hergestellt werden. Neuer Versuch (${attempt})`
+        : 'Es konnte keine Verbindung zum Server hergestellt werden. Bitte versuchen Sie es später erneut.';
+    this.notification$.next(message);
+  }
 }

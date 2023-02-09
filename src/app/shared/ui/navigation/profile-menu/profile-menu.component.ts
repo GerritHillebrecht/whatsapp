e22f4e0cb5d26a90d94695e4a25872fae8f842b2
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-09 12:59:28 +0100

    removed dark toggle from profile menu

--- src/app/shared/ui/navigation/profile-menu/profile-menu.component.ts
+++ src/app/shared/ui/navigation/profile-menu/profile-menu.component.ts
@@ -1,23 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Input } from '@angular/core';
 import { WhatsappUser } from '@pages/whatsapp/interface';
 import { AvatarComponent } from '@shared/ui/avatar/avatar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
-import { DarkmodeToggleComponent } from '@shared/ui/toogle/darkmode';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDividerModule } from '@angular/material/divider';
 import { Logout } from '@auth/store/authentication.actions';
+import { WhatsappState } from '@whatsapp/store';
 
 @Component({
   selector: 'app-profile-menu',
   standalone: true,
   imports: [
     CommonModule,
-    DarkmodeToggleComponent,
     AvatarComponent,
     MatMenuModule,
     MatIconModule,
@@ -28,7 +26,7 @@ import { Logout } from '@auth/store/authentication.actions';
   styleUrls: ['./profile-menu.component.scss'],
 })
 export class ProfileMenuComponent {
-  @Select((state: any) => state.authentication.whatsappUser)
+  @Select(WhatsappState.whatsappUser)
   whatsappUser$!: Observable<WhatsappUser | null>;
 
   constructor(private store: Store) {}

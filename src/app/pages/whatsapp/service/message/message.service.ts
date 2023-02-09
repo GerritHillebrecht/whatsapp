020b35a3a6e73766f5f16281dcb9fc256441a609
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-09 12:48:57 +0100

    reworked the sendMessage method

--- src/app/pages/whatsapp/service/message/message.service.ts
+++ src/app/pages/whatsapp/service/message/message.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 
 import { Apollo } from 'apollo-angular';
-import { map, Observable, take } from 'rxjs';
-import { WhatsappMessage } from '@whatsapp/interface';
+import { map, Observable, take, tap } from 'rxjs';
+import { WhatsappMessage, WhatsappUser } from '@whatsapp/interface';
 import { MessageComposeService } from '../message-compose/message-compose.service';
 import { Store } from '@ngxs/store';
+import uniqid from 'uniqid';
+
 import {
+  MessageQueryResult,
+  MessageQueryVariables,
+  MESSAGE_QUERY,
   SYNCHRONIZATION_QUERY,
-  SyncQueryResult,
+  SyncQueryResult as syncResult,
+  SyncQueryVariables as syncVars,
 } from '../synchronisation/sync.query';
 import {
   MessageCreateResult,
@@ -15,15 +21,20 @@ import {
   MESSAGE_CREATE_MUTATION,
 } from '@pages/test-bench/personal-testing/graphql-optimistic-response/typedDocumentNodes.graphql';
 
-import uniqid from 'uniqid';
-
 import {
   ReadStatusUpdateResult,
   ReadStatusUpdateVariables,
   READ_STATUS_UPDATE_MUTATION,
 } from './message.graphql';
+import { WhatsappContactState, WhatsappState } from '@whatsapp/store';
+import { ApolloCache } from '@apollo/client/core';
 
-
+interface OptimisticResponseVariables {
+  uuid: string;
+  body: string;
+  sender: WhatsappUser;
+  receiver: WhatsappUser;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -36,82 +47,135 @@ export class MessageService {
   ) {}
 
   sendMessage(): Observable<WhatsappMessage> {
+    const [sender, receiver] = [
+      this.store.selectSnapshot(WhatsappState.whatsappUser)!,
+      this.store.selectSnapshot(WhatsappContactState.selectedContact)!,
+    ];
+
+    const uuid = uniqid();
     const body = this.compose.messageControl.value;
-    this.compose.messageControl.reset();
-    const [receiver, sender] = this.store.selectSnapshot((state) => [
-      state.whatsapp.selectedContact,
-      state.authentication.whatsappUser,
-    ]);
-    const variables = {
-      uuid: uniqid(),
+
+    const variables: MessageCreateVariables = {
+      uuid,
       body,
       receiverId: receiver.id,
       senderId: sender.id,
     };
 
+    console.log('variables', variables);
+
+    const optimisticResponse: MessageCreateResult =
+      this.createOptimisticResponse({
+        uuid,
+        body,
+        sender,
+        receiver,
+      });
+
     return this.apollo
       .mutate<MessageCreateResult, MessageCreateVariables>({
         mutation: MESSAGE_CREATE_MUTATION,
         variables,
-        optimisticResponse: {
-          __typename: 'Mutation',
-          saveMessage: {
-            __typename: 'Message',
-            id: 0,
-            uuid: variables.uuid,
-            body,
-            image: '',
-            isRead: false,
-            deliveryStatus: 'pending',
-            sender: {
-              __typename: 'User',
-              ...sender,
-            },
-            receiver: {
-              __typename: 'User',
-              ...receiver,
-            },
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          },
-        },
-        update: (cache, result) => {
-          const cachedData = cache.readQuery<SyncQueryResult, { id: number }>({
-            query: SYNCHRONIZATION_QUERY,
-            variables: {
-              id: this.store.snapshot().authentication.whatsappUser.id,
-            },
-          });
+        optimisticResponse,
+        update: (cache, { data }) => {
+          if (!data) return;
 
-          // If there is no data in the cache, we don't need to update it
-          if (!cachedData || !result.data) return;
+          const { id } = this.store.selectSnapshot(WhatsappState.whatsappUser)!;
+          const { saveMessage: message } = data;
 
-          const { messages, contacts } = cachedData;
-          const { saveMessage } = result.data;
-
-          const message = {
-            ...saveMessage,
-            sender: this.store.snapshot().authentication.whatsappUser,
-            receiver: this.store.snapshot().whatsapp.selectedContact,
+          const cacheQueryOptions = {
+            query: MESSAGE_QUERY,
+            variables: { id },
           };
 
-          const data = {
-            contacts,
-            messages: [message, ...messages],
-          };
+          cache.updateQuery<MessageQueryResult, MessageQueryVariables>(
+            cacheQueryOptions,
+            (data: MessageQueryResult | null): MessageQueryResult | null => {
+              if (!data) {
+                return data;
+              }
+              if (data.messages.some((m) => m.uuid === message.uuid)) {
+                return data;
+              }
 
-          // Write the new data to the cache
-          cache.writeQuery({
-            query: SYNCHRONIZATION_QUERY,
-            variables: { id: message.sender.id },
-            data,
-          });
+              return {
+                ...data,
+                messages: [message, ...(data!.messages || [])],
+              };
+            }
+          );
         },
       })
       .pipe(
+        tap(() => this.compose.messageControl.reset()),
         map((result: any) => result.data.sendMessage),
         take(1)
       );
+  }
+
+  private updateCache(cache: ApolloCache<any>, data: MessageCreateResult) {
+    // const { id } = this.store.selectSnapshot(WhatsappState.whatsappUser)!;
+    // const cacheQueryOptions = {
+    //   query: MESSAGE_QUERY,
+    //   variables: { id },
+    // };
+    // cache.updateQuery(cacheQueryOptions, (data: MessageQueryResult | null) => {
+    //   return {
+    //     __typename: 'Query',
+    //     ...data,
+    //   };
+    // });
+    // const cachedData = cache.readQuery<
+    //   MessageQueryResult,
+    //   MessageQueryVariables
+    // >({
+    //   query: MESSAGE_QUERY,
+    //   variables: { id: whatsappUser.id },
+    // });
+    // if (!cachedData) return;
+    // const { messages } = cachedData;
+    // const { saveMessage } = data;
+    // const message = {
+    //   ...saveMessage,
+    //   sender: whatsappUser,
+    //   receiver: this.store.snapshot().whatsapp.selectedContact,
+    // };
+    // cache.writeQuery({
+    //   query: MESSAGE_QUERY,
+    //   variables: { id: whatsappUser.id },
+    //   data: {
+    //     messages: [message, ...messages],
+    //   },
+    // });
+  }
+
+  private createOptimisticResponse({
+    uuid,
+    body,
+    sender,
+    receiver,
+  }: OptimisticResponseVariables): MessageCreateResult {
+    return {
+      __typename: 'Mutation',
+      saveMessage: {
+        __typename: 'Message',
+        id: 0,
+        uuid: uuid,
+        body,
+        image: '',
+        deliveryStatus: 'pending',
+        sender: {
+          __typename: 'User',
+          ...sender,
+        },
+        receiver: {
+          __typename: 'User',
+          ...receiver,
+        },
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    };
   }
 
   updateReadStatus(messageIds: number[]) {
@@ -122,6 +186,4 @@ export class MessageService {
       })
       .pipe(map(({ data }) => data?.updateReadStatus));
   }
-
-
 }

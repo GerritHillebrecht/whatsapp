7ca5a14428e03a32f86bf8e8e5726295ed9c80ab
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-09 12:51:32 +0100

    Separation of contacts and messages

--- src/app/pages/whatsapp/service/synchronisation/synchronisation.service.ts
+++ src/app/pages/whatsapp/service/synchronisation/synchronisation.service.ts
@@ -1,125 +1,117 @@
 import { Injectable } from '@angular/core';
-import { WatchQueryFetchPolicy } from '@apollo/client/core';
+import { AuthenticationState } from '@auth/store';
 import { NotificationService } from '@core/services/notification';
 import { Store } from '@ngxs/store';
+import { WhatsappContactState } from '@whatsapp/store';
+import { UpdateReadStatus } from '@whatsapp/store/message/message.actions';
 import { Apollo } from 'apollo-angular';
 import {
   catchError,
   delay,
-  filter,
-  interval,
   Observable,
   retryWhen,
   skip,
   take,
   takeUntil,
   tap,
-  timeout,
+  throwError,
 } from 'rxjs';
 import { map } from 'rxjs';
 import {
   WhatsappContact,
   WhatsappMessage,
   WhatsappUser,
-  WhatsappMessageQueryDto,
 } from '../../interface';
 import { MessageHelperService } from '../message/message-helper.service';
 import {
-  SYNCHRONIZATION_QUERY,
-  SyncQueryResult,
-  SyncQueryVariables,
+  ContactQueryResult,
+  ContactQueryVariables,
+  CONTACT_QUERY,
+  MessageQueryResult,
+  MessageQueryVariables,
+  MESSAGE_QUERY,
 } from './sync.query';
+
+export interface MessageMap {
+  [key: WhatsappContact['id']]: WhatsappMessage[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class SynchronisationService {
-  takeUntilCondition = this.store
-    .select(({ authentication }) => authentication.whatsappUser)
+  private retryCount = 1;
+
+  private takeUntilCondition = this.store
+    .select(AuthenticationState.firebaseUser)
     .pipe(skip(1));
 
+  private errorHandler = (error: any) => {
+    this.notification.serverConnectionError(this.retryCount++);
+    return throwError(() => new Error('Error while fetching data'));
+  };
+
+  private retryWhenCondition = (errors: Observable<Error>) =>
+    errors.pipe(delay(5000), take(5));
+
   constructor(
-    private apollo: Apollo,
     private store: Store,
-    private helper: MessageHelperService,
+    private apollo: Apollo,
+    private support: MessageHelperService,
     private notification: NotificationService
   ) {}
 
-  syncDataWithServer(id: number): Observable<{
-    contacts: WhatsappContact[];
-    messageMap: Map<number, WhatsappMessage[]>;
-  }> {
-    const syncQueryOptions = {
-      query: SYNCHRONIZATION_QUERY,
-      variables: { id },
-      fetchPolicy: 'cache-and-network' as WatchQueryFetchPolicy,
-    };
-
+  fetchMessages(whatsAppUserId: number): Observable<WhatsappMessage[]> {
     return this.apollo
-      .watchQuery<SyncQueryResult, SyncQueryVariables>(syncQueryOptions)
+      .watchQuery<MessageQueryResult, MessageQueryVariables>({
+        query: MESSAGE_QUERY,
+        variables: { id: whatsAppUserId },
+      })
       .valueChanges.pipe(
         takeUntil(this.takeUntilCondition),
-        catchError(() => {
-          this.notification.notification$.next(
-            'Daten konnten nicht geladen werden, erneuter Versuch in 5 Sekunden'
-          );
-          throw new Error('Error while fetching data');
-        }),
-        retryWhen((errors) => errors.pipe(delay(5000), take(5))),
-        filter((result) => Boolean(result.data)),
-        map(({ data: { messages, contacts } }) => {
-          const messageMap = this.processMessages(messages, contacts);
-          const contactsData = this.processContacts(messageMap, contacts);
-          this.helper.markAsRead(
-            Array.from(messageMap, ([key, value]) => value).flat() || []
-          );
-          return { contacts: contactsData, messageMap };
-        })
+        catchError(this.errorHandler),
+        retryWhen(this.retryWhenCondition),
+        map(({ data }) => data.messages || []),
+        map((messages) => messages.map((msg) => this.support.mapToMsg(msg))),
+        tap((messages) => this.markAsRead(messages))
       );
   }
 
-  private processMessages(
-    messages: WhatsappMessageQueryDto[],
-    contacts: WhatsappUser[]
-  ): Map<number, WhatsappMessage[]> {
-    return contacts.reduce((messageMap, { id }) => {
-      const contactMessages = this.filterAndMapMessages(messages, id);
-      messageMap.set(id, contactMessages);
-      return messageMap;
-    }, new Map());
+  fetchContacts(whatsAppUserId: number): Observable<WhatsappUser[]> {
+    return this.apollo
+      .watchQuery<ContactQueryResult, ContactQueryVariables>({
+        query: CONTACT_QUERY,
+        variables: { id: whatsAppUserId },
+      })
+      .valueChanges.pipe(
+        takeUntil(this.takeUntilCondition),
+        catchError(this.errorHandler),
+        retryWhen(this.retryWhenCondition),
+        map(({ data }) => data.contacts || [])
+      );
   }
 
-  private processContacts(
-    messages: Map<number, WhatsappMessage[]>,
-    contacts: WhatsappUser[]
-  ): WhatsappContact[] {
-    return contacts.map((contact) => {
-      const contactMessages = messages.get(contact.id) || [];
-      const lastMessage = contactMessages.at(0) || null;
-      const unreadMessages = contactMessages.filter(
-        (message) =>
-          message.sender.id === contact.id &&
-          message.deliveryStatus === 'delivered'
-      ).length;
+  private markAsRead(messages: WhatsappMessage[]): void {
+    const selectedUserId = this.store.selectSnapshot(
+      WhatsappContactState.selectedContact
+    )?.id;
 
-      return {
-        ...contact,
-        messages: contactMessages,
-        lastMessage,
-        unreadMessages,
-      };
-    });
-  }
-  // Hepler Functions //////////////////////////////////////////////////////////
+    if (!selectedUserId) return;
 
-  private filterAndMapMessages(
-    messages: WhatsappMessageQueryDto[],
-    contactId: number
-  ) {
-    return messages
-      .filter(({ sender, receiver }) =>
-        [sender.id, receiver.id].includes(contactId)
-      )
-      .map((message) => this.helper.mapDtoToMessage(message));
+    const unreadMessageIds = messages
+      .filter(({ sender, deliveryStatus, isMine }) => {
+        return (
+          !isMine &&
+          sender.id === selectedUserId &&
+          deliveryStatus === 'delivered'
+        );
+      })
+      .map(({ id }) => id);
+
+    console.log('unreadMessageIds', unreadMessageIds);
+
+    if (unreadMessageIds.length > 0) {
+      this.store.dispatch(new UpdateReadStatus(unreadMessageIds));
+    }
   }
 }

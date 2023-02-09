48a956a04a79532d077c7ce67034a523e76613a3
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-09 12:57:58 +0100

    Swapped state action to new state

--- src/app/pages/whatsapp/ui/contact/contact.component.ts
+++ src/app/pages/whatsapp/ui/contact/contact.component.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { WhatsappContact } from '@pages/whatsapp/interface';
 import { AvatarComponent } from '@shared/ui/avatar';
 import { Store } from '@ngxs/store';
-import { SelectContact } from '@pages/whatsapp/store';
 import { ScreenSizeService } from '@core/services/screen-size';
 import { Router, RouterModule } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
+import { SelectWhatsappContact } from '@whatsapp/store/contact/contact.actions';
 
 @Component({
   selector: 'app-contact',
@@ -32,6 +32,6 @@ export class ContactComponent {
     if (await firstValueFrom(this.screenSize.twSm$)) {
       this.router.navigate(['whatsapp', 'chat']);
     }
-    return this.store.dispatch(new SelectContact(this.contact!));
+    return this.store.dispatch(new SelectWhatsappContact(this.contact!));
   }
 }

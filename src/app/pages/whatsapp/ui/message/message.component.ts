747f195ebe2780a32e266c7e07abba198e2946a6
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-13 23:37:04 +0100

    added fetching of images from storage

--- src/app/pages/whatsapp/ui/message/message.component.ts
+++ src/app/pages/whatsapp/ui/message/message.component.ts
@@ -1,12 +1,13 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Input } from '@angular/core';
 import { WhatsappMessage, WhatsappUser } from '@pages/whatsapp/interface';
 import { MessageDeliveryStatusComponent } from '../message-delivery-status';
-import { MessageService } from '@whatsapp/service';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { WhatsappState } from '@whatsapp/store';
+import { StorageService } from '@whatsapp/service/storage/storage.service';
+import { MessageImage } from '@whatsapp/interface/whatsapp.message.interface';
 
 @Component({
   selector: 'app-message',
@@ -15,22 +16,29 @@ import { WhatsappState } from '@whatsapp/store';
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.scss'],
 })
-export class MessageComponent implements AfterViewInit {
-  @ViewChild('messageRef')
-  messageRef: ElementRef<HTMLDivElement> | undefined;
+export class MessageComponent implements OnInit {
+  @Select(WhatsappState.whatsappUser)
+  user$: Observable<WhatsappUser> | undefined;
 
   @Input()
   message: WhatsappMessage | undefined;
 
-  @Select(WhatsappState.whatsappUser)
-  user$: Observable<WhatsappUser> | undefined;
+  @Input()
+  index: number = 0;
 
-  constructor(private service: MessageService) {}
+  imageSrc: MessageImage[] | undefined;
+  imageFallbackSrc: string | undefined;
 
-  ngAfterViewInit(): void {
-    this.messageRef!.nativeElement.addEventListener('contextmenu', (event) => {
-      event.preventDefault();
-      // this.service.showContextMenu(event, this.message!);
-    });
+  constructor(private storageService: StorageService) {}
+
+  ngOnInit(): void {
+    if (this.message?.image) {
+      this.storageService
+        .getImages(this.message.image, this.message.uuid)
+        .then((images) => {
+          this.imageSrc = images;
+          this.imageFallbackSrc = images.at(-1)?.filename;
+        });
+    }
   }
 }

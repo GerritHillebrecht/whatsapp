cacf65dd978387f954b578238eae7cd80559760f
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-13 23:27:15 +0100

    added a type for Message Images

--- src/app/pages/whatsapp/interface/whatsapp.message.interface.ts
+++ src/app/pages/whatsapp/interface/whatsapp.message.interface.ts
@@ -1,20 +1,30 @@
+import {
+  ImageSizes,
+  ImageType,
+} from '@whatsapp/service/storage/storage.service';
 import { gql } from 'apollo-angular';
 import { WhatsappUser } from './whatsapp.user.interface';
 
-export interface WhatsappMessageQueryDto {
+export interface WhatsappMessageDto {
   __typename?: 'Message';
   id: number;
   uuid: string;
   body: string;
   sender: WhatsappUser;
   receiver: WhatsappUser;
-  image?: string | null;
+  image: string | null;
   deliveryStatus: WhatsappMessageDeliveryStatus;
   createdAt: Date;
   updatedAt: Date;
 }
 
-export interface WhatsappMessage extends WhatsappMessageQueryDto {
+export type MessageImage = {
+  filename: string;
+  size: ImageSizes;
+  type: ImageType;
+};
+
+export interface WhatsappMessage extends WhatsappMessageDto {
   isMine: boolean;
 }
 

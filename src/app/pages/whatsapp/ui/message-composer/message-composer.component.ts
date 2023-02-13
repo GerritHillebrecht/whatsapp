bf33395720e1d53aadcfe1b7d6d454de7785adcb
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-13 23:37:46 +0100

    added image Input view toggle

--- src/app/pages/whatsapp/ui/message-composer/message-composer.component.ts
+++ src/app/pages/whatsapp/ui/message-composer/message-composer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MessageComposeService, MessageService } from '@whatsapp/service';
 import { TextareaComponent, EmojiSelectorComponent } from './input';
+import { ImageSelectorComponent } from './input/image-selector/image-selector.component';
+import { Select, Store } from '@ngxs/store';
+import { SendMessage } from '@whatsapp/store/message/message.actions';
+import { MessageHelperService } from '@whatsapp/service/message/message-helper.service';
+import { WhatsappContactState } from '@whatsapp/store';
+import { distinctUntilChanged, Observable } from 'rxjs';
+import { WhatsappContact } from '@whatsapp/interface';
 
 @Component({
   selector: 'app-message-composer',
@@ -17,6 +24,7 @@ import { TextareaComponent, EmojiSelectorComponent } from './input';
 
     TextareaComponent,
     EmojiSelectorComponent,
+    ImageSelectorComponent,
 
     ReactiveFormsModule,
     FormsModule,
@@ -24,13 +32,29 @@ import { TextareaComponent, EmojiSelectorComponent } from './input';
   templateUrl: './message-composer.component.html',
   styleUrls: ['./message-composer.component.scss'],
 })
-export class MessageComposerComponent {
+export class MessageComposerComponent implements OnInit {
+  @Select(WhatsappContactState.selectedContact)
+  selectedContact$: Observable<WhatsappContact> | undefined;
+
   constructor(
+    protected helper: MessageHelperService,
     protected compose: MessageComposeService,
-    protected message: MessageService
+    protected message: MessageService,
+    private store: Store
   ) {}
 
+  ngOnInit(): void {
+    this.selectedContact$?.pipe(distinctUntilChanged()).subscribe(() => {
+      this.compose.messageForm.reset({ text: '', image: '' });
+      this.helper.showImageSelector$.next(false);
+    });
+  }
+
+  toggleImageSelector() {
+    this.helper.showImageSelector$.next(!this.helper.showImageSelector$.value);
+  }
+
   sendMessage() {
-    this.message.sendMessage().subscribe();
+    this.store.dispatch(new SendMessage());
   }
 }

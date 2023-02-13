98ceecd90967a01d8c350e01993918e13b45c9ac
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-13 23:42:48 +0100

    swapped to firebaseUser

--- src/app/shared/ui/navigation/profile-menu/profile-menu.component.ts
+++ src/app/shared/ui/navigation/profile-menu/profile-menu.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { WhatsappUser } from '@pages/whatsapp/interface';
 import { AvatarComponent } from '@shared/ui/avatar/avatar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,7 +8,8 @@ import { Observable } from 'rxjs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDividerModule } from '@angular/material/divider';
 import { Logout } from '@auth/store/authentication.actions';
-import { WhatsappState } from '@whatsapp/store';
+import { AuthenticationState } from '@auth/store';
+import { User } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-profile-menu',
@@ -26,8 +26,8 @@ import { WhatsappState } from '@whatsapp/store';
   styleUrls: ['./profile-menu.component.scss'],
 })
 export class ProfileMenuComponent {
-  @Select(WhatsappState.whatsappUser)
-  whatsappUser$!: Observable<WhatsappUser | null>;
+  @Select(AuthenticationState.firebaseUser)
+  firebaseUser$!: Observable<User | null>;
 
   constructor(private store: Store) {}
 

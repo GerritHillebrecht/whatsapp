066d82754914d6926cb3667b4a7dbc1c7c68d6a3
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-13 23:21:15 +0100

    added the 3x size for widescreens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      }
+    },
   },
   plugins: [],
 };
